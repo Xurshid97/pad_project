c859f615f6e2f7304f79cfc54f5070882c1268c0
Xurshid97 <xurshedrabo@gmail.com>
2024-05-10 16:43:55 +0200

    selenium added and also some basic button clicks are done

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ pip install requests
     2, Beautiful Soup sits on top of famous Python parsers like LXML and HTML
  -->
 pip install beautifulsoup4
+
+<!-- 
+    control web browsers programmatically, enabling tasks such as web scraping, automated testing, and web application interaction. Selenium supports various web browsers, including Chrome, Firefox, Safari, and Edge, making it a versatile tool for browser automation.
+ -->
+ pip install -U selenium
--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,32 @@
 import requests 
 from bs4 import BeautifulSoup 
 
-r = requests.get('https://www.failory.com/failures')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-soup = BeautifulSoup(r.content, 'html.parser') 
-print(soup.prettify()) 
+# r = requests.get('https://www.failory.com/failures')
+
+# soup = BeautifulSoup(r.content, 'html.parser') 
+# s = soup.find('div', class_='w-dyn-list') 
+
+# list_of_country_links = s.find_all('a') 
+
+browser = webdriver.Firefox()
+browser.get('https://www.failory.com/failures')
+
+def close_pop_up():
+    try:
+        pop_up_close_button = browser.find_element(By.CLASS_NAME, "popup-icon-close")
+        pop_up_close_button.click()
+    except:
+        print("Popup did not appear within the specified time.")
+
+def click_country_button(country_button_class_name):
+    country_button = browser.find_element(By.ID, country_button_class_name)
+    country_button.click()
+
+
+close_pop_up()
+click_country_button('w-node-c78e2231-b714-552c-3658-765240b39563-a65bf16a')
