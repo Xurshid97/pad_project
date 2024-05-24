2e8cc451ed4aa467fdeae6ed70e2e0bab858c925
Xurshid97 <xurshedrabo@gmail.com>
2024-05-24 17:47:27 +0200

    data scrapping is done with uploading to file

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
-
-python -m venv myenv 
+python -m venv myenv
 myenv\Scripts\activate
 
-*** to make HTTP requests to specified URI using GET, POST, PUT, PATCH, or HEAD ***
-pip install requests 
+**_ to make HTTP requests to specified URI using GET, POST, PUT, PATCH, or HEAD _**
+pip install requests
 
-*** 1, converts incoming records to Unicode and outgoing forms to UTF-8 ***
-*** 2, Beautiful Soup sits on top of famous Python parsers like LXML and HTML ***
+**_ 1, converts incoming records to Unicode and outgoing forms to UTF-8 _**
+**_ 2, Beautiful Soup sits on top of famous Python parsers like LXML and HTML _**
 pip install beautifulsoup4
 
+**_ control web browsers programmatically, enabling tasks such as web scraping, automated testing, and web application interaction. Selenium supports various web browsers, _** **_ including Chrome, Firefox, Safari, and Edge, making it a versatile tool for browser automation._**
 
- ***   control web browsers programmatically, enabling tasks such as web scraping, automated testing, and web application interaction. Selenium supports various web browsers, *** *** including Chrome, Firefox, Safari, and Edge, making it a versatile tool for browser automation.***
- 
- pip install -U selenium
+pip install -U selenium
 
+**_ pandas library _**
+pip install pandas
 
+**_ pip install openpyxl _**
+pip install openpyxl
 
  <!-- 
-    while extracting data, I just get the h3 which represents the names of the startups
+    while scrapping for each start_up 
   -->
--- scrapper.py
+++ scrapper.py
@@ -3,60 +3,50 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-# r = requests.get('https://www.failory.com/failures')
-
-# soup = BeautifulSoup(r.content, 'html.parser') 
-# s = soup.find('div', class_='w-dyn-list') 
-
-# list_of_country_links = s.find_all('a') 
 
 browser = webdriver.Firefox()
 browser.get('https://www.failory.com/failures')
 
-def close_pop_up():
-    try:
-        pop_up_close_button = browser.find_element(By.CLASS_NAME, "popup-icon-close")
-        pop_up_close_button.click()
-        print('pop up appeared')
-    except:
-        print("Popup did not appear within the specified time.")
+def get_data_from_country_page(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
 
+    list_of_failed_startups = []
+    list_of_main_data = soup.find_all(class_="listicle-list")
+    # get listicle-list-item of each list_of_main_data
+    for main_data in list_of_main_data:
+        listicle_list_items = main_data.find_all(class_="listicle-list-item")
+        one_failed_startup = []
+        for listicle_list_item in listicle_list_items:
+            # get first listicle-list-category and failed-startup-list-information, make a key value pairs, and append to list_of_failed_startups
+            category = listicle_list_item.find(class_="listicle-list-category")
+            information = listicle_list_item.find(class_="failed-startup-list-information")
+            if category and information:
+                one_failed_startup.append({category.get_text(strip=True): information.get_text(strip=True)})
+        list_of_failed_startups.append(one_failed_startup)
+    return list_of_failed_startups
 
-def click_country_button(browser, country_button_class_name):
-    try:
-        country_button = browser.find_element(By.ID, country_button_class_name)
-        country_button.click()
-        return browser.current_url
-    except:
-        close_pop_up()
+# get 3rd main-page-container class from a list of same classes
+main_page_container = browser.find_elements(By.CLASS_NAME, "main-page-container")[2]
+# get all a tags href from that main-page-container
+urls_of_countries = [country_link.get_attribute('href') for country_link in main_page_container.find_elements(By.TAG_NAME, "a")]
 
+final_data = []
+# for each country, get the data from the country page
+for url_of_country in urls_of_countries:
+    final_data.append(get_data_from_country_page(url_of_country))
 
-# enter every country and save their data to a file
-url_of_country = click_country_button(browser, 'w-node-c78e2231-b714-552c-3658-765240b39563-a65bf16a')
+browser.quit()
 
-r = requests.get(url_of_country)
-soup = BeautifulSoup(r.content, 'html.parser')
+# convert final_data to json
+import json
+with open('final_data.json', 'w') as f:
+    json.dump(final_data, f)
 
-list_of_failed_startups = []
+# convert final_data to csv
+import pandas as pd
+df = pd.DataFrame(final_data)
+df.to_csv('final_data.csv', index=False)
 
-items = soup.find_all(class_="listicle-startup-collection-item")
-
-for item in items:
-    h3_tag = item.find('h3', class_='listicle-h3')
-    info = {}
-    information_of_start_up = ['founder', 'country', 'industry', 'started_in', 'closed_in', '#_of_employees', 'funding_amount', 'cause']
-    informations = item.find_all(class_='failed-startup-list-information')
-    for i in range(len(informations)):
-        if i < len(information_of_start_up):
-            info[information_of_start_up[i]] = informations[i].get_text(strip=True)
-    each_start_up = {}
-    if h3_tag:
-        startup_name = h3_tag.get_text(strip=True)
-        each_start_up[startup_name] = info
-    list_of_failed_startups.append(each_start_up)
-print(list_of_failed_startups)
-
-# some mistakes on extracting data, some startups do not have n of employees, also, some has no funding solve it by its name
+df.to_excel('final_data.xlsx', index=False)
